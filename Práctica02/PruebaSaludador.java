283549874b960c1fa17c4339a7bc743a9e229f5d
JPflores20 <99496617+JPflores20@users.noreply.github.com>
2022-08-26 07:49:52 -0500

    Práctica02_07:46
    En el pdf dice que creemos la clase "PruebaSaludador.java", sin embargo en el código dice qué le pongamos "public class TestSaludador{}", ésto es incorrecto pues cómo sabemos, el nombre del archivo tiene que ser igual al nombre de la clase ósea "public class PruebaSaludador{}"

---
+++ Práctica02/PruebaSaludador.java
@@ -0,0 +1,6 @@
+public class PruebaSaludador{//Clase Prueba (en el pdf dice TestSaludador)
+    public static void main(String[] args){//Método principal (main)
+        Saludador alumno = new Saludador();
+        alumno.saludar();
+    }
+}
---
+++ Práctica02/Saludador.java
@@ -0,0 +1,6 @@
+public class Saludador{//Definición de la clase
+    public void saludar(){//llave qué inicia el método
+        System.out.println("Hola Alumno de POO");
+        
+    }//llave que cierra el método saludar
+}//llave que cierra la clase

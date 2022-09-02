d085be93c4978e589615e8a98c116353e206d951
JPflores20 <99496617+JPflores20@users.noreply.github.com>
2022-09-02 08:44:31 -0500

    Práctica04_08:40
    Buenos días profesor, le envió la actividad de ésta semana, por motivos de salud no pude asistir presencialmente hoy a clases, ya que desde anoche tengo síntomas de gripa y preferí no ir, pero le hago entrega de la práctica 04 de ésta semana, sin mas por el momento le deseo buen día :)
    
    PD. noté un error en el pdf, decía qué modificáramos la clase "PruebaPersonaje," y al final del pdf, venía que compiláramos la clase "TestPersonaje"

---
+++ Practica04/Personaje.java
@@ -0,0 +1,44 @@
+public class Personaje {//Definición clase principal
+    private String nombre; //Nombre privado porqué es un dato privado no público
+    private int edad;//edad privada, porque es un dato personal privado, no publico
+
+
+//*******************Modificaciónes práctica 04************************* */
+    //constructor con edad por defecto =0
+    public Personaje(String nombre){
+        this.nombre = nombre;
+        edad=0;//edad por defecto es 0
+    }
+//********************Termina modificación******************************* */
+
+
+
+    public void setNombre(String nombre){//recibe una cadena nombre como argumento y modifica el atributo nombre como dicho argumento
+        this.nombre= nombre;
+    }//cierra método
+
+    //regresa una cadena con el valor del nombre (atributo de clase)
+    public String getNombre(){
+        return nombre;
+    }//cierra método
+
+    public void saludar(){//método saludar
+        System.out.println("Hola Alumno de POO "+ nombre);
+
+    }//cierra método
+
+    
+//*******************Modificaciónes práctica 04************************* */
+    public int getEdad(){
+        return edad;
+    }
+
+    public boolean setEdad(int edad){
+        if((edad>0) && (edad<120)){
+            this.edad=edad;
+            return true;
+        }
+        return false;
+    }
+//********************Termina modificación******************************* */
+}

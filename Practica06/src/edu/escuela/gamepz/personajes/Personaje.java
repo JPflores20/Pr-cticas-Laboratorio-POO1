8e61482c84d855d9127d9612538481ff565e1e12
JPflores20 <99496617+JPflores20@users.noreply.github.com>
2022-09-09 08:22:11 -0500

    Práctica06_08_21
    Problemas con package e import, corregir

--- Practica06/src/edu/escuela/gamepz/personajes/Personaje.java
+++ Practica06/src/edu/escuela/gamepz/personajes/Personaje.java
@@ -1,46 +1,64 @@
-package com.patito.poo1pract.personajes;
+package edu.escuela.gamepz.personajes; //problemas con package, corregir
 
 public class Personaje {//Definición clase principal
     private String nombre; //Nombre privado porqué es un dato privado no público
     private int edad;//edad privada, porque es un dato personal privado, no publico
 
 
-//*******************Modificaciónes práctica 04************************* */
-    //constructor con edad por defecto =0
+//*******************Modificaciónes práctica 04************************* */  
+    //constructor con edad por defecto =0  
     public Personaje(String nombre){
-        this.nombre = nombre;
+        this.nombre= nombre;
         edad=0;//edad por defecto es 0
-    }
+    }//cierra método
 //********************Termina modificación******************************* */
 
 
+//modificar el método setNombre para qué solo se pueda cambiar el nombre si la longitud de la cadena recibida es mayor a 5 y menor a 25
+    public boolean setNombre(String nombre){
+        int longitudNombre= nombre.length();
+        if((longitudNombre>5) && (longitudNombre<25)){//mayor a 5 y menor a 25
+           this.nombre= nombre;
+            return true;
+        }
+        else{
+            return false;
+        }
 
-    public void setNombre(String nombre){//recibe una cadena nombre como argumento y modifica el atributo nombre como dicho argumento
-        this.nombre= nombre;
     }//cierra método
-
+//********************Termina modificación******************************* */
     //regresa una cadena con el valor del nombre (atributo de clase)
     public String getNombre(){
         return nombre;
     }//cierra método
 
     public void saludar(){//método saludar
-        System.out.println("Hola Alumno de POO "+ nombre);
-
+        System.out.println("Hola alumno de POO "+ nombre);
     }//cierra método
 
-    
 //*******************Modificaciónes práctica 04************************* */
     public int getEdad(){
         return edad;
-    }
+    }//cierra método
 
     public boolean setEdad(int edad){
-        if((edad>0) && (edad<120)){
-            this.edad=edad;
+        if((edad > 0) && (edad < 120)){
+            this.edad= edad;
             return true;
         }
-        return false;
-    }
+        else{
+            return false;
+        }
+    }//cierra método
 //********************Termina modificación******************************* */
-}
+    
+
+//Agregar método getDetalle(), el cual regresa una cadena con el nombre y la edad separados por un tabulador (\t)
+    public String getDetalle(){
+        return nombre+"\t"+edad;
+    }
+    
+//********************Termina modificación******************************* */
+}//cierra clase
+
+
---
+++ Practica06/src/edu/escuela/gamepz/pruebas/PruebaPersonaje.java
@@ -0,0 +1,83 @@
+package edu.escuela.gamepz.pruebas;//problema package, corregir
+import edu.escuela.gamepz.personajes.Personaje;
+
+
+public class PruebaPersonaje {
+
+    public static void main(String[] args) {
+
+// modificar clase PruebaPersonaje para cear 5 instancias de la Personaje en el método principal 
+
+        //Personaje per01
+        Personaje per01= new Personaje("Almendra");
+        per01.setEdad(200);
+        per01.getDetalle();
+        //imprimir instancia con el método getDetalle
+        System.out.println(per01.getDetalle());
+
+        //Personaje per02
+        Personaje per02= new Personaje("Ricardo");
+        per02.setEdad(10);
+        per02.getDetalle();
+        //imprimir instancia con el método getDetalle
+        System.out.println(per02.getDetalle());
+
+        //Personaje per03
+        Personaje per03= new Personaje("Bianca");
+        per03.setEdad(1);
+        per03.getDetalle();
+        //imprimir instancia con el método getDetalle
+        System.out.println(per03.getDetalle());
+
+        //Personaje per04
+        Personaje per04= new Personaje("Leonel");
+        per04.setEdad(-10);
+        per04.getDetalle();
+        //imprimir instancia con el método getDetalle
+        System.out.println(per04.getDetalle());
+
+        //Personaje per05
+        Personaje per05= new Personaje("Hector");
+        per05.setEdad(500);
+        per05.getDetalle();
+        //imprimir instancia con el método getDetalle
+        System.out.println(per05.getDetalle());
+
+        
+        //agregar 5 instanciaas de la clase personaje 
+        Personaje per06= per05;
+        Personaje per07= per04;
+        Personaje per08= per03;
+        Personaje per09= per02;
+        Personaje per10= per01;
+
+//cambio de nombres
+        per06.setNombre("Francisco Xavier Zepeda Flores");// cambiar el nombre de la instancia per06 por "Francisco Xavier Zepeda Flores"
+        per08.setNombre("Dayan");// cambiar el nombre de la instancia per08 por "Dayan"
+        per10.setNombre("Axel Fabian Eduardo David");// cambiar el nombre de la instancia per10 por "Axel Fabian Eduardo David"
+
+        // cambiar el valor de las edades usando setEdan(n) (n= NumeroAleatorio) con un numero n aleatorio etre 0 y 1000 de las instancias  per07 y per09
+        int NumeroAleatorio= (int)(Math.random()*1000);//random crea numero aleatorio
+        per07.setEdad(NumeroAleatorio);
+
+        int NumeroAleatorio2= (int)(Math.random()*1000);
+        per09.set
+        
+        (NumeroAleatorio2);
+
+        //imprimir tres guiones(---)
+        System.out.println("---");
+
+        //imprimir los datos de las primeras 5 instancias utilizando el método getDetalle
+        System.out.println(per01.getDetalle());
+        System.out.println(per02.getDetalle());
+        System.out.println(per03.getDetalle());
+        System.out.println(per04.getDetalle());
+        System.out.println(per05.getDetalle());
+        
+        
+        
+        
+    }  
+}
+

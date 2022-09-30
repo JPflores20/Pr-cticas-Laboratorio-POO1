9404de9c25bc9a2a5a5ee04695bc40ac7a44933b
JPflores20 <99496617+JPflores20@users.noreply.github.com>
2022-09-30 10:28:45 -0500

    Parcial1 10:28 a.m.

---
+++ Parcial1/src/mx/parc1/soft/mx/parc1/soft/TestSeries/Series.java
@@ -0,0 +1,31 @@
+package mx.parc1.soft.TestSeries;
+import mx.parc1.soft.TestSeries.*;
+public class Series {
+     
+    public static void main(String[] args){
+        int i, numero = 1, contador;
+
+
+
+
+
+        
+        System.out.println(" Los números primos son:  ");
+        while(numero <= 100){
+            contador = 0;
+            i = 2;
+            while(i <= numero/2 ){
+                if(numero % i == 0){
+                contador++;
+                break;
+                }
+             i++;
+            }
+            if(contador == 0 && numero != 1 ){
+                System.out.print(numero + " ");
+            }
+         numero++;
+        }
+    }
+        
+}
--- Parcial1/src/mx/parc1/soft/mx/parc1/soft/TestSeries/TestSeries.java
+++ Parcial1/src/mx/parc1/soft/mx/parc1/soft/TestSeries/TestSeries.java
@@ -15,28 +15,7 @@ public class TestSeries {
 
 
 
-  /* PRUEBA IMPRIMIR PRIMOS DEL 1 AL 100
-        int i, numero = 1, contador;
-      
-        
-         while(numero <= m){
-             contador = 0;
-              i = 2;
-               while(i <= numero/2 ) {
-              if(numero % i == 0){
-                 contador++;
-                 break; //SI SE CUMPLE ROMPO EL CICLO
-                 }
-                i++;
-             }
-             if(contador == 0 && numero != 1 ){
-                  System.out.print(numero + " ");
-             }
-              numero++;
-         }
-         System.out.println(" Los numeros primos del 1 al 100 son: ", s.primos(m));
-
-         */
+ 
 
         
 

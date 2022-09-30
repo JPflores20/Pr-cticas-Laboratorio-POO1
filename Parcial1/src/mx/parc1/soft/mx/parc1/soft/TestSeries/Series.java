13e821e54b2dcc1e8506b40dbc8f07dd6489e40d
JPflores20 <99496617+JPflores20@users.noreply.github.com>
2022-09-30 11:00:57 -0500

    Parcial1 11:00 a.m.

--- Parcial1/src/mx/parc1/soft/mx/parc1/soft/TestSeries/Series.java
+++ Parcial1/src/mx/parc1/soft/mx/parc1/soft/TestSeries/Series.java
@@ -1,29 +1,42 @@
 package mx.parc1.soft.TestSeries;
 import mx.parc1.soft.TestSeries.*;
 public class Series {
-
-//***************************************************************** */
-    public static void main(String[] args){
-
-        int i, numero = 1, contador;
-        while(numero <= 100){
-            contador = 0;
-            i = 2;
-            while(i <= numero/2 ){
-                if(numero % i == 0){
-                contador++;
-                break;
-                }
-             i++;
-            }
-            if(contador == 0 && numero != 1 ){
-                System.out.print(numero + " ");
-            }
-         numero++;
-        }
+    int x=100;
+    int n;
+    
+    public void Serie(int n){
+     this.n =n;
     }
-//********************************************************************* */     
+    
+    public void genNumero(int n){
+     while (n<100){
+         n= (int)(Math.random()"x"10);
+         System.out.println("Número: "+ n);
+     } 
+    
+    }
 
+ //***************************************************************** */
+ public void primo(){
+
+    int i, numero = 1, contador;
+    while(numero <= n){
+        contador = 0;
+        i = 2;
+        while(i <= numero/2 ){
+            if(numero % i == 0){
+            contador++;
+            break;
+            }
+         i++;
+        }
+        if(contador == 0 && numero != 1 ){
+            System.out.print(numero + " ");
+        }
+     numero++;
+    }
+}
+//********************************************************************* */     
 
 
 }

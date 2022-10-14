62fd0e1b8c284d7d6b8066f99dab6dc97e4aeaa1
JPflores20 <99496617+JPflores20@users.noreply.github.com>
2022-10-14 09:07:56 -0500

    Practica09 09:07

--- Practica09/mx/desasof/inmobiliaria/casas/Urbana.java
+++ Practica09/mx/desasof/inmobiliaria/casas/Urbana.java
@@ -1,6 +1,22 @@
 package mx.desasof.inmobiliaria.casas;
 
 public class Urbana extends Casa {
+    private int banios;
 
+    public Urbana(String tipo, int hab, float costo){
+        super(tipo,hab,costo);
+    }
+
+    public Urbana (String tipo, int banios){
+        super(tipo);
+        this.banios=banios;
+    }
     
+    public int hashCode(){
+        return(super.hashCode() * banios * 23);
+    }
+
+    public boolean equals(Casa ObjetoUrbana){
+        return super.equals(ObjetoUrbana) && ObjetoUrbana instanceof Urbana;
+    }
 }

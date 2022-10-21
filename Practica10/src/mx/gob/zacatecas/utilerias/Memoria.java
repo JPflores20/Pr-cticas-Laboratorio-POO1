8a0ae2f893646668a23800d44037e6cfcc3e5850
JPflores20 <99496617+JPflores20@users.noreply.github.com>
2022-10-21 08:15:09 -0500

    Practica10 08:15

--- Practica10/src/mx/gob/zacatecas/utilerias/Memoria.java
+++ Practica10/src/mx/gob/zacatecas/utilerias/Memoria.java
@@ -6,12 +6,26 @@ public enum Memoria {
     DDR3("Laptop nueva",553f,40),
     DDR4("Gamer", 1600f, 4);
 
+    private String tipo;
+    private float velocidad;
+    private int pines;
+    
     private Memoria(String tipo,float velocidad, int pines){
         this.tipo = tipo;
         this.velocidad=velocidad;
         this.pines=pines;
     
 
+    }
+
+    public String getTipo(){
+        return tipo;
+    }
+    public float getVelocidad(){
+        return velocidad;
+    }
+    public int getPines(){
+        return pines;
     }
 
     
---
+++ Practica10/src/mx/gob/zacatecas/utilerias/Tipo.java
@@ -0,0 +1,9 @@
+package mx.gob.zacatecas.utilerias;
+
+public enum Tipo {
+    SERVER("Servidor"),
+    WORKSTATION("Estacion de trabajo"),
+    DESKTOP("Escritorio"),
+    LAPTOP("Portatil");
+
+}

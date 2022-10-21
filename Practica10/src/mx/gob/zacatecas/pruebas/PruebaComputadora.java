d8dd99ed9ee553ce1ba5aa1daf4db3fb7c271bf4
JPflores20 <99496617+JPflores20@users.noreply.github.com>
2022-10-21 09:21:48 -0500

    Practica10 09:21

---
+++ Practica10/src/mx/gob/zacatecas/pruebas/PruebaComputadora.java
@@ -0,0 +1,14 @@
+package mx.gob.zacatecas.pruebas;
+import mx.gob.zacatecas.enumerados.equipos.*;
+public class PruebaComputadora {
+    public static void main(String[] args){
+    Computadora DELL =new Computadora("DELL", DDR);
+    Computadora HP= new Computadora("HP", DDR2);
+    Computadora LENOVO= new Computadora("LENOVO", DDR3);
+    Computadora ASUS = new Smallfactor("ASUS", DDR4);
+
+
+    }
+
+    
+}

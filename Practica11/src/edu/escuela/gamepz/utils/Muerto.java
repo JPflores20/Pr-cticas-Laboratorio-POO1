220b7aafd584e6a51b5895ff232e7e5782cd5700
JPflores20 <99496617+JPflores20@users.noreply.github.com>
2022-10-28 09:51:31 -0500

    Práctica11 09:51

---
+++ Practica11/src/edu/escuela/gamepz/utils/Muerto.java
@@ -0,0 +1,8 @@
+package edu.escuela.gamepz.utils;
+
+public interface Muerto {
+    
+        public int MAX_ZOMBIES = 10;// atributo con valor de 10
+        public void comer();//método comer que no regresa ningun valor
+    
+}

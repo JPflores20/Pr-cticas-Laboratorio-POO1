387df47539e68ed4da80e75d792cfa9ee82f96f0
JPflores20 <99496617+JPflores20@users.noreply.github.com>
2022-11-25 10:51:50 -0600

    Práctica15 10:51

--- Práctica15/src/mx/com/hilos/corredores/hilos/RunHilos.java
+++ Práctica15/src/mx/com/hilos/corredores/hilos/RunHilos.java
@@ -8,10 +8,10 @@ public class RunHilos {
         char ch1 = txt1.getText().charAt(0);
         char ch2 = txt2.getText().charAt(0);
 
-        if(txt1.getText() ==("") ){
+        if(txt1.getText().length()==0){
             ch1= 'E';
         }
-        if(txt2.getText() ==("")){
+        if(txt2.getText().length()==0){
             ch2= 'M';
         }
 
---
+++ Práctica15/src/mx/com/hilos/corredores/pruebas/Ventana.java
@@ -0,0 +1,5 @@
+package mx.com.hilos.corredores.pruebas;
+
+public class Ventana {
+
+}

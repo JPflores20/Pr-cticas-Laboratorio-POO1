af32937d22aac4b7e889938001c03104ab77be6a
JPflores20 <99496617+JPflores20@users.noreply.github.com>
2022-11-25 08:25:47 -0600

    Práctica15 08:25

--- Práctica15/src/mx/com/hilos/corredores/ventanas/Ventana.java
+++ Práctica15/src/mx/com/hilos/corredores/ventanas/Ventana.java
@@ -1,9 +1,10 @@
 import javax.swing.JFrame;
 
-
-
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
 public class Ventana extends JFrame {
     JLabel IbITitulo, IbIElem1, IbIElem2, hElem1, hElem2;
-    JtextField txtElem1, txtElem2;
+    JTextField txtElem1, txtElem2;
     JButton btnStart;
 }

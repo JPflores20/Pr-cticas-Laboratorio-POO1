bf1a31903f9aec34254d3a4108c98bd5fd9356d7
JPflores20 <99496617+JPflores20@users.noreply.github.com>
2022-11-25 09:53:28 -0600

    Práctica15 09:53

--- Práctica15/src/mx/com/hilos/corredores/hilos/RunHilos.java
+++ Práctica15/src/mx/com/hilos/corredores/hilos/RunHilos.java
@@ -1,7 +1,12 @@
 package mx.com.hilos.corredores.hilos;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
 
 public class RunHilos {
-    public void iniciar(){
-        
+    public static void iniciar (JTextField txt1, JTextField txt2, ){
+
+        char ch1 = txt1.getText().charAt(0);
+        char ch2 = txt2.getText().charAt(0);
+
     }
 }

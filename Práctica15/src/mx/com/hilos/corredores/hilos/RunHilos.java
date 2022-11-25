505cf142d2af7372040e58fb22e543cab6fecab7
JPflores20 <99496617+JPflores20@users.noreply.github.com>
2022-11-25 10:08:36 -0600

    Práctica15 10:08

--- Práctica15/src/mx/com/hilos/corredores/hilos/RunHilos.java
+++ Práctica15/src/mx/com/hilos/corredores/hilos/RunHilos.java
@@ -19,6 +19,12 @@ public class RunHilos {
         String salida2 = "" + ch2;
         elem1.setText(salida1);
         elem2.setText(salida2);
+        Letra hilo1 = new Letra(elem1, ch1);
+        Letra hilo2 = new Letra(elem1, ch2);
+        Thread run1 = new Thread(hilo1);
+        Thread run2 = new Thread(hilo2);
+        run1.start();
+        run2.start();
 
 
     }

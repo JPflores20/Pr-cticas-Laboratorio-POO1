208e8e38bb86fe343851b62274cc9623f0ef172c
JPflores20 <99496617+JPflores20@users.noreply.github.com>
2022-11-25 08:21:24 -0600

    Práctica15 08:21

---
+++ Práctica15/src/mx/com/hilos/corredores/ventanas/Ventana.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+
+
+
+public class Ventana extends JFrame {
+    JLabel IbITitulo, IbIElem1, IbIElem2, hElem1, hElem2;
+    JtextField txtElem1, txtElem2;
+    JButton btnStart;
+}

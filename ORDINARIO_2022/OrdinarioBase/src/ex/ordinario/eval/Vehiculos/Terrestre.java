e72c6fc06dc5c697fbc76ce79e354a3b053356eb
JPflores20 <99496617+JPflores20@users.noreply.github.com>
2022-12-06 10:26:03 -0600

    Ordinario2022-2 10:25

--- ORDINARIO_2022/OrdinarioBase/src/ex/ordinario/eval/Vehiculos/Terrestre.java
+++ ORDINARIO_2022/OrdinarioBase/src/ex/ordinario/eval/Vehiculos/Terrestre.java
@@ -1,3 +1,12 @@
-public class Terrestre{
-	int llantas;
+public class Terrestre extends Vehiculo{//hereda de vehiculo
+	int llantas; //atributo entero llamado llantas
+
+	public Terrestre(String marca, int modelo, float costo, int llantas){
+		super(marca, modelo, costo);
+		this.llantas = llantas;
+	}
+
+	public String toString(){//agregar atributo llantas
+		return super.toString() + "\t" + llantas;
+	}
 }

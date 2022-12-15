fb7518a4422052a991edf7562300907b9e5e755c
JPflores20 <99496617+JPflores20@users.noreply.github.com>
2022-12-15 10:38:14 -0600

    Extra2022 10:38

--- Extra22022/src/mx/edu/elextra/extraeval/ventanas/Ventana.java
+++ Extra22022/src/mx/edu/elextra/extraeval/ventanas/Ventana.java
@@ -1,4 +1,7 @@
 package mx.edu.elextra.extraeval.ventanas;
+import mx.edu.elextra.extraeval.acciones.Procesos;
+import javax.swing.*;
+import java.awt.*;
 public class Ventana extends JFrame{
 	public Ventana(){
 		initComponents();
@@ -55,36 +58,62 @@ public class Ventana extends JFrame{
 		// Crear el Menú Item miMarca "Marca"
 		JMenuItem miMarca=new JMenuItem("Marca");
 		// Crear el Menú Item miCosto "Costo"
-
+		JMenuItem miCosto=new JMenuItem("Costo");
 		// Crear la etiqueta lbMarca "Marca"
+		JLabel lbMarca=new JLabel("Marca");
 		// Crear la etiqueta lbMarca "Costo"
+		JLabel lbCosto=new JLabel("Costo");
 		// Crear la etiqueta lbRamP "Ram/Proc"
-		
+		JLabel lbRamP=new JLabel("Ram/Proc");
 		// Crear el campo de texto txMarca de tamaño 20
+		JTextField txMarca=new JTextField(20);
 		// Crear el campo de texto txCosto de tamaño 20
+		JTextField txCosto=new JTextField(20);
 		// Crear el campo de texto txRamP de tamaño 20
-
+		JTextField txRamP=new JTextField(20);
 		// Crear el botón btnPrev " <- "
+		JButton btnPrev=new JButton(" <- ");
 		// Crear el botón btnNext " -> "
-
+		JButton btnNext=new JButton(" -> ");
 		// Crear la etiqueta lbIdx "0/0"
-		
+		JLabel lbIdx=new JLabel("0/0");
 		String espacios = "       ";
 		// Crear la etiqueta Label ordenando lbTipoOrd espacios+"Tipo de Ordenamiento"+espacios
-		
+		JLabel lbTipoOrd=new JLabel(espacios+"Tipo de Ordenamiento"+espacios);
 		// Agregar al btnPrev la clase anónima que manda llamar al método siguientes:
+		btnPrev.addActionListener(new ActionListener(){
+			public void actionPerformed(Event e){
 				Procesos.getPrev(txMarca, txCosto, txRamP, lbIdx);
+			}
+		}); 
+				
 
 		// Agregar al btnNext la clase anónima que manda llamar al método siguientes:
+		btnNext.addActionListener(new ActionListener(){
+			public void actionPerformed(Event e){
 				Procesos.getNext(txMarca, txCosto, txRamP, lbIdx);
+			}
+		});
+				
 
 		// Agregar al miMarca la clase anónima que manda llamar a los métodos siguientes: 
+		miMarca.addActionListener(new ActionListener(){
+			public void actionPerformed(Event e){
 				lbTipoOrd.setText(espacios+"Ordenados por Marca"+espacios);
 				Procesos.sortMarca();
+			}
+		});
+				
 
+		
 		// Agregar al miCosto la clase anónima que manda llamar a los métodos siguientes: 
+		miMarca.addActionListener(new ActionListener(){
+			public void actionPerformed(Event e){
 				lbTipoOrd.setText(espacios+"Ordenados por Costo"+espacios);
 				Procesos.sortCosto();
+			}
+		});		
+
 		setSize(330,350);
 	}
 	JMenuBar mb;

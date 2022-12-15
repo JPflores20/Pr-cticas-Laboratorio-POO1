63548913adbf5c87cecd0fa0f93a0f285af2b6df
JPflores20 <99496617+JPflores20@users.noreply.github.com>
2022-12-15 11:26:01 -0600

    Extra2022 11:25

--- Extra22022/src/mx/edu/elextra/extraeval/acciones/Procesos.java
+++ Extra22022/src/mx/edu/elextra/extraeval/acciones/Procesos.java
@@ -1,6 +1,11 @@
 package mx.edu.elextra.extraeval.acciones;
 import javax.swing.*;
 import java.awt.*;
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.*;
+import mx.edu.elextra.extraeval.dispositivos.*;
 public class Procesos{
 	ArrayList<Dispositivo> lista=new ArrayList();
 	private int idx = 0; //indice para moverse en el ArrayList
@@ -18,7 +23,7 @@ public class Procesos{
 			idx=idx-1;
 		}
 		// establecer la etiqueta del indice en el valor que corresponda; ejemplo 1/20
-		indice.setText(idx+"/"+lista.size);
+		indice.setText(idx+"/"+lista.size());
 		llenarCampos(marca, costo, ramP);
 	}
 	public void getNext(JTextField marca, JTextField costo, JTextField ramP , JLabel indice){
@@ -27,19 +32,19 @@ public class Procesos{
 			JOptionPane.showMessageDialog(null, "Lista vacia");
 			return;
 		}
-		if (idx<lista.size) {
+		if (idx<lista.size()) {
 			idx=idx+1;
 		}
 		// sumar a idx 1; en caso de ser igual al tamaño de la lista no hacer el incremento
 		// establecer la etiqueta del indice en el valor que corresponda; ejemplo 20/20
-		indice.setText(idx+"/"+lista.size);
+		indice.setText(idx+"/"+lista.size());
 		llenarCampos(marca, costo, ramP);
 	}
 	private void llenarCampos(JTextField marca, JTextField costo, JTextField ramp){
-		Dispositivo dd=lista[idx];
+		Dispositivo dd = lista[idx];
 		// Con los valores marca costo y ram/procesador mostrar en las cajas de texto
 		marca.setText(dd.getMarca());
-		costo.setText(dd.getCosto());
+		costo.setText(""+dd.getCosto());
 		// Considerar que puede ser una Computadora o un Celular
 	}
 	public void sortMarca(){
@@ -47,30 +52,56 @@ public class Procesos{
 			return;
 		}
 		else{
-			lista.sort
+			Collections.sort(lista, new porMarca());
 		}
 	}
 	public void sortCosto(){
-		// Si la lista está vacía, no se ordena
-		// Ordenar por Marca
+		if (lista==null) {
+			return;
+		}
+		else{
+			Collections.sort(lista, new porCosto());
+		}
 	}
 	public void openFile(){
 		System.out.println("Nombre del archivo:");
 		// Por medio de un Scanner obtener el nombre de un archivo
+		Scanner elS=new Scanner(System.in);
+		String name=elS.nextLine();
+		File f=new File(name);
 		// Verificar si el archivo existe
-		// No existe se llama al siguiente JOptionPane y termina el método
+		if (f.exists()) {
+			BufferedReader lector=new BufferedReader(new FileReader(f));
+				String linea;
+				while ((linea = lector.readLine()) != null) {
+   					 agregarLinea(linea);
+  				 }
+		}
+		else{
 			JOptionPane.showMessageDialog(null, "El archivo no existe");
+			return;
+		}
+		// No existe se llama al siguiente JOptionPane y termina el método
+			
 		// Si el archivo existe crear un BufferedReader para leer el contenido del archivo
 		// Con cada linea del archivo llamar al método
-				agregarLinea(linea);
+				
 	}
-	private void agregarLinea(String){
+	private void agregarLinea(String line){
 		// Separar la linea por comas ','
+		String[] separos= line.split(",");
 		// si no tiene 4 elementos mostrar el siguiente JOptionPane y terminar el método
+		if (separos.length!=4) {
 			JOptionPane.showMessageDialog(null, "Formato incorrecto");
+			return;
+		}
+		else{
+
+			
 		// Con los datos separados por coma:
 		// Si el primer elemento es una c, crear una Computadora y agregar a la lista
 		// Si el primer elemento es una p, crear un Celular y agregar a la lista
 		// para lo anterior se debe de verificar con un try que los datos se pueden convertir a numéricos
+		}
 	}
 }

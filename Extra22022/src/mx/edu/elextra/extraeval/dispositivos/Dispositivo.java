ea03b449f4b535fae1b95e55dfb800c02f7a4d49
JPflores20 <99496617+JPflores20@users.noreply.github.com>
2022-12-15 09:11:01 -0600

    Extra2022 09:10

--- Extra22022/src/mx/edu/elextra/extraeval/dispositivos/Dispositivo.java
+++ Extra22022/src/mx/edu/elextra/extraeval/dispositivos/Dispositivo.java
@@ -1,9 +1,34 @@
 public class Dispositivo {
 
-	String marca;
-	float costo;
+	private String marca;
+	private float costo;
 
-	public Dispositivo(String float){
-
+	public Dispositivo(String marca, float costo){
+		this.marca=marca;
+		this.costo=costo;
 	}
+	public String toString(){
+		return marca + '\t' + costo;
+	}
+
+	public String getMarca(){
+		return marca;
+	}
+	public float getCosto(){
+		return costo;
+	}
+	//fin método get atributos
+
+	//orden natural
+	public int compareTo(Vehiculo vehiculo1){//uso de compare to para orden
+		if ((this.marca.compareTo(vehiculo1.marca)) != 0){
+			return (this.marca.compareTo(vehiculo1.marca));
+		}
+
+		if (this.modelo == modelo){
+			return (this.modelo>vehiculo1.modelo)?(1):(-1);
+		}
+		return (this.costo > vehiculo1.costo)?(1):(-1);
+	}
+	
 }
